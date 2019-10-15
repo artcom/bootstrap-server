6025adcaf5e352611c38a266db1d83cfa20a35ec
Mirko Geissler <mirko.geissler@artcom.de>
2019-10-15 10:12:14 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# Bootstrap Server
+
+A HTTP server serving basic bootstrap data as JSON object.
+
+## Local Usage
+
+```bash
+npm install --global bunyan
+npm install
+
+export BACKEND_HOST=<backendHost>
+export HTTP_BROKER_URI=<httpBrokerUri>
+export TCP_BROKER_URI=<tcpBrokerUri>
+export WS_BROKER_URI=<wsBrokerUri>
+export CONFIG_SERVER_URI=<configServerUri>
+npm start | bunyan
+```
+
+The bootstrap data can now be queried at `http://localhost:3000/<device>`.
+
+## Deployment
+
+The service can be deployed with Dokku.
---
+++ src/main.ts
@@ -0,0 +1,40 @@
+import * as express from "express"
+import * as cors from "cors"
+import * as Logger from "bunyan"
+
+const PORT = process.env.PORT || 3000
+
+const BOOTSTRAP_DATA = {
+  backendHost: process.env.BACKEND_HOST,
+  httpBrokerUri: process.env.HTTP_BROKER_URI,
+  tcpBrokerUri: process.env.TCP_BROKER_URI,
+  wsBrokerUri: process.env.WS_BROKER_URI,
+  configServerUri: process.env.CONFIG_SERVER_URI
+}
+
+const logger = Logger.createLogger({
+  name: "bootstrapServer",
+  level: "debug",
+  serializers: { error: Logger.stdSerializers.err }
+})
+
+function bootstrap(request: express.Request, response: express.Response) {
+  const device = request.params.device
+
+  logger.info({ device, ip: request.ip }, "Returning bootstrap data")
+
+  response.json({
+    ...BOOTSTRAP_DATA,
+    deviceTopic: `devices/${device}`,
+    device
+  })
+}
+
+const app = express()
+app.use(cors())
+app.set("trust proxy", true)
+app.get("/:device", bootstrap)
+
+app.listen(PORT, () => {
+  logger.info({ port: PORT, bootstrapData: BOOTSTRAP_DATA }, "Bootstrap Server up and running")
+})
